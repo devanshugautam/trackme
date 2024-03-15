3fc18439af5c79b9fdae83838c507eadd2167679
devanshuimg <devanshug.img@gmail.com>
2024-03-15 12:51:18 +0530

    socket

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
-/* eslint-disable no-undef */
+//  Local import
 const { userModel } = require('../dbModel');
-
+const { getSpeedLimit } = require('./speedLimit');
 /**
  * configuration of sockets
  *
@@ -19,6 +19,10 @@ module.exports = (io) => {
             const info = JSON.parse(data);
             console.log('Message received: ', info);
             const update = await userModel.findOneAndUpdate({ _id: info.userId }, { latitude: info.lat, longitude: info.long, coordinates: [info.long, info.lat] }, { new: true });
+            const findSpeedLimit = await getSpeedLimit();
+            if (findSpeedLimit.success && (info.speed > findSpeedLimit.data.speedLimit)) {
+                io.to(info.userId).emit('overSpeed', { success: true, message: `Over-speed alert: Vehicle exceeded the speed limit of 70 km/h. Current speed: ${info.speed} km/h..`, userId: info.userId });
+            }
             if (update) {
                 console.log('updated user', update);
                 io.to(info.userId).emit('sendcheck', { success: true, message: 'user location fetched successfully.', userId: info.userId || 'ajshlasd' });
