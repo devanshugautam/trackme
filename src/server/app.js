cf259b998d32ef5fc8ba223a5d6e610ee7f72339
devanshuimg <devanshug.img@gmail.com>
2024-03-15 10:37:58 +0530

    socket

--- src/server/app.js
+++ src/server/app.js
@@ -37,7 +37,7 @@ io.on('connection', (socket) => {
     socket.on('getuserLocation', (data) => {
         const info = JSON.parse(data);
         console.log('Message received: ', info);
-        socket.emit('sendcheck', { success: true, message: 'user location fetched successfully.', userId: info.userId || 'ajshlasd' });
+        io.to(info.userId).emit('sendcheck', { success: true, message: 'user location fetched successfully.', userId: info.userId || 'ajshlasd' });
     });
     socket.on('disconnect', () => {
         console.log('User disconnected');
