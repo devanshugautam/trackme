91d97924e0a4a6eab4eee27fb8c8aae44979e031
devanshuimg <devanshug.img@gmail.com>
2024-03-26 13:23:11 +0530

    check

--- src/services/socket.js
+++ src/services/socket.js
@@ -25,66 +25,73 @@ module.exports = (io) => {
             const info = JSON.parse(data);
             console.log('Message received: ', info);
 
-            // Update user location in the database
-            const update = await userModel.findOneAndUpdate(
-                { _id: info.userId },
-                { latitude: info.lat, longitude: info.long, coordinates: [info.long, info.lat] },
-                { new: true }
-            );
+            if (info?.userId) {
+                // Update user location in the database
+                const update = await userModel.findOneAndUpdate(
+                    { _id: info.userId },
+                    { latitude: info.lat, longitude: info.long, coordinates: [info.long, info.lat] },
+                    { new: true }
+                );
 
-            // Get the speed limit for the location
-            const findSpeedLimit = await getSpeedLimit();
+                // Get the speed limit for the location
+                const findSpeedLimit = await getSpeedLimit();
 
-            // Emit over-speed alert if speed exceeds the limit
-            if (findSpeedLimit.success && info.speed > findSpeedLimit.data.speedLimit) {
-                await query.create(userOverSpeedModel, {
-                    speed: info.speed,
-                    longitude: info.long,
-                    latitude: info.lat,
-                    coordinates: [info.long, info.lat],
-                    userId: info.userId,
-                    vehicleType: findSpeedLimit.data.vehicleType,
-                    laneType: findSpeedLimit.data.laneType,
-                    speedLimit: findSpeedLimit.data.speedLimit,
-                    speedLimitId: findSpeedLimit.data._id
-                });
-                io.to(info.userId).emit('overSpeed', {
-                    success: true,
-                    message: `Over-speed alert: Vehicle exceeded the speed limit of ${findSpeedLimit.data.speedLimit} km/h. Current speed: ${info.speed} km/h.`,
-                    userId: info.userId
-                });
-            }
+                // Emit over-speed alert if speed exceeds the limit
+                if (findSpeedLimit.success && info.speed > findSpeedLimit.data.speedLimit) {
+                    await query.create(userOverSpeedModel, {
+                        speed: info.speed,
+                        longitude: info.long,
+                        latitude: info.lat,
+                        coordinates: [info.long, info.lat],
+                        userId: info.userId,
+                        vehicleType: findSpeedLimit.data.vehicleType,
+                        laneType: findSpeedLimit.data.laneType,
+                        speedLimit: findSpeedLimit.data.speedLimit,
+                        speedLimitId: findSpeedLimit.data._id
+                    });
+                    io.to(info.userId).emit('overSpeed', {
+                        success: true,
+                        message: `Over-speed alert: Vehicle exceeded the speed limit of ${findSpeedLimit.data.speedLimit} km/h. Current speed: ${info.speed} km/h.`,
+                        userId: info.userId
+                    });
+                }
 
-            // Emit success message for location update
-            if (update) {
-                // console.log('updated user', update);
-                io.to(info.userId).emit('sendcheck', { success: true, message: 'user location fetched successfully.', userId: info.userId || 'ajshlasd' });
+                // Emit success message for location update
+                if (update) {
+                    // console.log('updated user', update);
+                    io.to(info.userId).emit('sendcheck', { success: true, message: 'user location fetched successfully.', userId: info?.userId });
+                }
             }
         });
 
         socket.on('reportAccident', async (data) => {
             const info = JSON.parse(data);
-            // console.log('data>>(reportAccident)>>>>>>>>>>>', data);
-            const reportAccident = await query.create(accidentReportModel, {
-                userId: info.userId,
-                speed: info.speed,
-                vehicleType: info.type,
-                coordinates: [info.long, info.lat],
-                latitude: info.lat,
-                longitude: info.long
-            });
-            if (reportAccident) {
-                io.to(info.userId).emit('accidentReport', {
-                    success: true,
-                    message: `User accident reported successfully.`,
-                    data: reportAccident
+            if (info?.userId) {
+                // console.log('data>>(reportAccident)>>>>>>>>>>>', data);
+                const reportAccident = await query.create(accidentReportModel, {
+                    userId: info.userId,
+                    speed: info.speed,
+                    vehicleType: info.type,
+                    coordinates: [info.long, info.lat],
+                    latitude: info.lat,
+                    longitude: info.long
                 });
+                if (reportAccident) {
+                    io.to(info.userId).emit('accidentReport', {
+                        success: true,
+                        message: `User accident reported successfully.`,
+                        data: reportAccident
+                    });
+                }
             }
         });
 
         socket.on('reportSOS', (data) => {
-            // {"lat":27.79847581,"long":76.64037606,"speed":0.0,"type":"car","userId":"65f291d2a5f8b8c75ec8bea1"}
-            console.log('data>>(reportSOS)>>>>>>>>>>>', data);
+            const info = JSON.parse(data);
+            if (info?.userId) {
+                // {"lat":27.79847581,"long":76.64037606,"speed":0.0,"type":"car","userId":"65f291d2a5f8b8c75ec8bea1"}
+                console.log('data>>(reportSOS)>>>>>>>>>>>', info);
+            }
         });
 
         // Handle user disconnection
